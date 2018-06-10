be66b70be4bf679490921bd72fef27973340b476
greenmouse147 <cdaisi@axelfoulon.fr>
2018-06-10 20:53:23 +0200

    genetic, v01

--- genetique2.py
+++ genetique2.py
@@ -11,8 +11,8 @@ global _CONTRAINTES_INITIALES
 _CONTRAINTES_INITIALES = list(contraintes)
 
 #debug vars
-global nbviable
-nbviable=0
+#global nbviable
+#nbviable=0
 
 def generate(nbobjet,popSize):
 	pop = [[[random.randint(0,1) for i in range(nbobjet)]] for j in range(popSize)]
@@ -64,7 +64,8 @@ def repair_solution(solution) :
 
 	#calcul du poids et de la valeur de la solution 
 	calculate_weight(solution)
-	
+	#print("\n\n\n")
+	#print(solution)
 	#print(_CONTRAINTES_INITIALES)
 	#print(solution)
 	#input()
@@ -82,46 +83,118 @@ def repair_solution(solution) :
 	
 	return solution
 
+def make_children(parents,childrens) :
+	#selectoner parents random
+	parent1 = parents.pop(random.randint(0,len(parents)-1))
+	parent2 = parents.pop(random.randint(0,len(parents)-1))
+	
+	#print("parent 1" + str(parent1))
+	#print("parent 2" + str(parent2))
+	
+	#selectionner le père partie droite 
+	father = random.randint(0,1)
+	 
+	
+	#déclaration de l'enfant
+	children = [[]]
+	
+	#création de l'enfant moitier père, moitier mère
+	if father == 0 :
+		children[0] = list(parent1[0][0:((len(parent1[0])//2))])
+		children[0].extend(list(parent2[0][((len(parent2[0])//2)):]))
+	elif father == 1: 
+		children[0] = list(parent2[0][0:((len(parent1[0])//2))])
+		children[0].extend(list(parent1[0][((len(parent2[0])//2)):]))
+	else : 
+		input("Erreur")
+	#debug
+	"""
+	#print("enfant 1" + str(children))
+	#print(len(children[0]))
+	#print(len(parent1[0]))
+	"""
+	
+	#mutation aléatoire
+	for i in range(0,30) : 
+		already_exist = 1 
+		while already_exist == 1  : 
+			already_exist = 0 #0=false
+			
+			rand_mut = random.randint(0,(len(children[0])-1))
+			#print(rand_mut)
+			if children[0][rand_mut] == 0 : 
+				children[0][rand_mut] =1
+			else : children[0][rand_mut] = 0
+			
+			for elem in pop :
+				if elem[0] == children[0] : 
+					already_exist = 1
+					break
+	
+	childrens.append(children)
 	
 #générer la population initiale	
-pop = generate(nbobjet,50)
+pop = generate(nbobjet,100)
 
-#debug
-"""
-for elem in pop :
-	print(elem)
-"""
-#vérifier la population initiale
 
-for elem in pop :
-	#print(elem)
-	repair_solution(elem)
+for z in range(0,5000):
+	#debug
+	"""
+	for elem in pop :
+		print(elem)
+	"""
+	#vérifier la population initiale
+	for elem in pop :
+		#print(elem)
+		repair_solution(elem)
 
-#debug
-"""
-print(_CONTRAINTES_INITIALES)
-for elem in pop :
-	print(check_viability(elem))
-	print(elem)
-"""
+	#debug
+	"""
+	print(_CONTRAINTES_INITIALES)
+	for elem in pop :
+		print(check_viability(elem))
+		print(elem)
+	"""
+		
+	#séléctionner l'élite
+		##classement des solutions
+	pop = list(sorted(pop,key=lambda l:l[2], reverse = True))
+		#selection des élites
+	parents=list(pop[0:50])
+	print(len(pop))
+		#selectionner les chanceux
+	for i in range (0,20):	
+		parents.append(pop[random.randint(50,(len(pop)-1))] )
+
+	"""
+	for elem in parents : 
+		print(elem)
+	print(len(parents))
+	"""
+
+	#Créer les enfants
+
+	childrens=[]
+	while len(parents) != 0 : 
+		make_children(parents,childrens)
+
+	#Vérifier les enfants
+	for elem in childrens :
+		#print(elem)
+		repair_solution(elem)
+		
+	#remplacer les plus faibles de la population
+		##ajouter les enfants à la population
+	for elem in childrens : 
+		pop.append(elem)
+
+		#classer la nouvelle population
+	pop = list(sorted(pop,key=lambda l:l[2], reverse = True))
+	pop = list(pop[0:-35])
+	#print("génération "+str(z)+" ; taille : " + str(len(pop)) )
+
+	#print("meilleure solution :" + str(pop[0][2]) )
+print("meilleure solution :" + str(pop[0][2]) )
 	
-#séléctionner l'élite
-	##classement des solutions
-pop = list(sorted(pop,key=lambda l:l[2], reverse = True))
+	#recommencer
 
-parents=list(pop[0:10])
-
-#selectionner les chanceux
-for i in range (0,10):	
-	parents.append( pop[random.randint(10,len(pop)-1)] )
-
-for elem in parents : 
-	print(elem)
-print(len(parents))
-#Créer les enfants
-
-#Vérifier les enfants
-
-#remplacer les plus faibles de la population
-
-#recommencer
